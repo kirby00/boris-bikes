afb3995550c6e32960bccbdab8e4cfbc8a33beaa
Max Kirby <mbkirby@gmail.com>
2013-03-04 17:10:13 +0000

    added Person class

--- boris.rb
+++ boris.rb
@@ -8,6 +8,7 @@ class Control
 
   def initialize
     @fleet = create_fleet
+    @people = create_people
   end
 
   def create_fleet
@@ -16,16 +17,47 @@ class Control
     fleet
   end
 
+  def create_people
+    people = []
+      10.times {people << Person.new}
+    people 
+  end
+
   def create_report
       total_bikes = @fleet.count 
       broken_bikes = @fleet.count {|b| b.is_broken?}
       working_bikes = total_bikes-broken_bikes
-      result = "total bikes #{total_bikes}\n" + "broken bikes #{broken_bikes}\n" + "working bikes #{working_bikes}"
+      total_people = @people.count
+      people_with_bikes = @people.count {|p| p.has_bike?}
+      people_without_bikes = total_people-people_with_bikes
+      result = "total bikes #{total_bikes}\n" + "broken bikes #{broken_bikes}\n" + "working bikes #{working_bikes}\n"
+      result + "total people #{total_people}\n" + "people with bikes #{people_with_bikes}\n" + "people without bikes #{people_without_bikes}"
   end    
 end
 
+class Person
+  
+  def initialize
+    @has_bike = rand(2)
+    #how does this work??
+  end  
+
+  def has_bike?
+    @has_bike == 1
+  end
+
+  def take_bike_from_station
+    #declare probability that person wants to take bike
+  end
+
+  def return_bike_to_station
+    #if you have a bike, you have to return it
+  end
+
+end
+
+
 class Bike
-  attr_accessor :broken
 
   def initialize
     @broken = rand(2)
@@ -39,3 +71,7 @@ end
 # Actual program running
 instance_of_control_class = Control.new
 puts instance_of_control_class.create_report
+
+# puts @people.inspect
+
+
