5abc49a765f108730ec48aba1ca736c5700dacb7
Max Kirby <mbkirby@gmail.com>
2013-03-04 16:22:17 +0000

    set up bike report

--- boris.rb
+++ boris.rb
@@ -6,24 +6,29 @@ class Control
 #run the bike scheme
 #create a report
 
-  def initialize()
+  def initialize
     @fleet = create_fleet
-    puts @fleet
   end
 
   def create_fleet
     fleet = []
-      10.times {fleet << Bike.new(rand(2))}
+      10.times {fleet << Bike.new}
     fleet
   end
 
+  def create_report
+      total_bikes = @fleet.count 
+      broken_bikes = @fleet.count {|b| b.is_broken?}
+      working_bikes = total_bikes-broken_bikes
+      result = "total bikes #{total_bikes}\n" + "broken bikes #{broken_bikes}\n" + "working bikes #{working_bikes}"
+  end    
 end
 
 class Bike
   attr_accessor :broken
 
-  def initialize(broken)
-    @broken=broken
+  def initialize
+    @broken = rand(2)
   end
 
   def is_broken?
@@ -31,6 +36,6 @@ class Bike
   end
 end
 
-
-control = Control.new
-
+# Actual program running
+instance_of_control_class = Control.new
+puts instance_of_control_class.create_report
